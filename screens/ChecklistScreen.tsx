612a0d42ce596c56ddec9028a621dc52a6890b7f
ikke09 <development@fam-bechtluft.de>
2023-04-21 16:44:30 +0200

    Introduce Types for Checklist

--- screens/ChecklistScreen.tsx
+++ screens/ChecklistScreen.tsx
@@ -1,52 +1,61 @@
-import { Box, Text, Checkbox, HStack, VStack } from 'native-base';
+import { Box, Text, Checkbox, HStack, VStack, Spinner } from 'native-base';
 import React, { useEffect, useState } from 'react';
 import { NavigationStackScreenProps } from '../types/types';
-import { API_BASE_URL } from '@env';
+import config from '../lib/config';
+import { Checklist } from '../types/checklist';
 
 export default function ChecklistScreen({ navigation }: NavigationStackScreenProps<'Checklist'>) {
-  const [checklist, setChecklist] = useState([]);
+  const [checklist, setChecklist] = useState<Checklist | null>(null);
   const [checked, setChecked] = useState(0);
 
   useEffect(() => {
     (async () => {
-      const res = await fetch(`${API_BASE_URL}/api/checklist`);
-      const data = await res.json();
-      setChecklist(data);
+      const url = `${config.ApiUrl}/api/checklist`;
+      try {
+        const res = await fetch(`${config.ApiUrl}/api/checklist`);
+        const data: Checklist = await res.json();
+        console.log(data);
+        setChecklist(data);
+      } catch (error) {
+        console.error(`Loading from ${url} failed`, error);
+      }
     })();
   }, []);
 
   return (
     <Box alignItems="center" w="100%">
-      <VStack my={4} space={4}>
-        {checklist.map((item, index) => (
-          <HStack
-            key={index}
-            w="90%"
-            space={4}
-            alignItems="center"
-            borderRadius="sm"
-            borderWidth={2}
-            borderColor="warmGray.200"
-            p={1}
-          >
-            <Text w="90%">{item}</Text>
-            <Checkbox
-              w="10%"
-              value={`${index}`}
-              accessibilityLabel="Checkbox für ein Checklistenelement"
-              defaultIsChecked={false}
-              onChange={(isChecked) => {
-                setChecked((prev) => (isChecked ? prev + 1 : prev - 1));
-              }}
-            />
-          </HStack>
-        ))}
-      </VStack>
-      {checklist.length === 0 && <Text>Checkliste konnte nicht geladen werden</Text>}
-      {checklist.length > 0 && (
-        <Text color="success.400">
-          {(checked / (checklist.length || 1)) * 100}% der Checkliste erfüllt
-        </Text>
+      {!checklist && <Spinner accessibilityLabel="Checkliste laden..." size="lg" />}
+      {checklist && (
+        <>
+          <VStack my={4} space={4}>
+            {checklist.map((item, index) => (
+              <HStack
+                key={index}
+                w="90%"
+                space={4}
+                alignItems="center"
+                borderRadius="sm"
+                borderWidth={2}
+                borderColor="warmGray.200"
+                p={1}
+              >
+                <Text w="90%">{item.description}</Text>
+                <Checkbox
+                  w="10%"
+                  value={`${index}`}
+                  accessibilityLabel="Checkbox für ein Checklistenelement"
+                  defaultIsChecked={false}
+                  onChange={(isChecked) => {
+                    setChecked((prev) => (isChecked ? prev + 1 : prev - 1));
+                  }}
+                />
+              </HStack>
+            ))}
+          </VStack>
+          <Text color="success.400">
+            {(checked / (checklist.length || 1)) * 100}% der Checkliste erfüllt
+          </Text>
+        </>
       )}
     </Box>
   );
---
+++ types/checklist.d.ts
@@ -0,0 +1,8 @@
+type ChecklistItem = {
+  description: string;
+  checked: boolean;
+};
+
+type Checklist = ChecklistItem[];
+
+export { ChecklistItem, Checklist };
