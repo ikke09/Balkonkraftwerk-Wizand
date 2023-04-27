41190e575a3e8b9f2dd692ba8fb0c6c065a8a018
ikke09 <development@fam-bechtluft.de>
2023-04-27 19:52:21 +0200

    Use inapp browser instead of webview to open PlugXR => Working AR!

--- lib/config.ts
+++ lib/config.ts
@@ -1,14 +1,16 @@
-import { API_PORT, API_BASE_URL, ENV } from '@env';
+import { API_PORT, API_BASE_URL, ENV, AR_MODEL_URL } from '@env';
 import Constants from 'expo-constants';
 
 type Config = {
   ApiUrl: string;
   Env: 'dev' | 'prod';
+  ARModelUrl: string;
 };
 
 const config: Config = {
   ApiUrl: `${API_BASE_URL}:${API_PORT}`,
   Env: ENV as 'dev' | 'prod',
+  ARModelUrl: AR_MODEL_URL,
 };
 
 const isProd = config.Env === 'prod';
