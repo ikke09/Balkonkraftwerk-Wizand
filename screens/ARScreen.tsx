5b1baa5d361823cdbd477972d528f111577e50cc
ikke09 <development@fam-bechtluft.de>
2023-04-27 18:58:08 +0200

    PV-Module via 3D WebView

--- screens/ARScreen.tsx
+++ screens/ARScreen.tsx
@@ -1,9 +1,25 @@
-import { Box, Button } from 'native-base';
+import { Spinner } from 'native-base';
 import React from 'react';
-import { useUserStateValue } from '../components/UserContext';
 import { NavigationStackScreenProps } from '../types/types';
+import { WebView } from 'react-native-webview';
 
 export default function ARScreen({ navigation }: NavigationStackScreenProps<'AR'>) {
-  const userContext = useUserStateValue();
-  return <Box alignItems="center">AR</Box>;
+  return (
+    <WebView
+      style={{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+      }}
+      containerStyle={{ backgroundColor: '#EEE' }}
+      originWhitelist={['*']}
+      source={{
+        html: '<iframe src="https://app.vectary.com/p/3uWkVTpRQbJ7DvhSJoKsKn" frameborder="0" width="100%" height="98%"></iframe>',
+      }}
+      startInLoadingState={true}
+      renderLoading={() => <Spinner />}
+      scalesPageToFit={true}
+      scrollEnabled={false}
+    />
+  );
 }
